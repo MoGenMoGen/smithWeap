5a2732bde165264f8977235ad4917faea1264e3a
yf <1060292708@qq.com>
2021-08-04 08:51:59 +0800

    修改

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // const serverURL = "http://192.168.0.16";
-// const serverURL = "https://saf.ae-smith.com";
-const serverURL = "http://418231ph80.qicp.vip/";
+const serverURL = "https://saf.ae-smith.com";
+// const serverURL = "http://238s4m.natappfree.cc/";
 const imgURL = "https://h5.guluint.com/ruicao/Image/";
 
 module.exports = {
